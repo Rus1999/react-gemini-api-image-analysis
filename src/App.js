7684e5a2f1c8d72f39c4e14c04d6cf5ca23a2a71
Rus1999 <punyarit07@gmail.com>
2024-08-25 01:43:33 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [ image, setImage ] = useState(null);
+  const [value, setValue] = useState("");
+  const [response, setResponse] = useState("");
+  const [error, setError] = useState("");
+
+  const surpriseOptions = [
+    'Dose the image have a whale?',
+    'Is the image fabulously pink?',
+    'Does the image have puppies?'
+  ];
+
+  const surprise = () => {
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)];
+    setValue(randomValue);
+  }
+
+  console.log(value);
+  
+  const uploadImage =  async(e) => {
+    setImage(e.target.files[0]);
+    const formData = new FormData();
+    formData.append('file', e.target.files[0]);
+    setImage(e.target.files[0]);
+
+    try {
+      const options = {
+        method: "POST",
+        body: formData
+      };
+
+      const response = await fetch('http://localhost:8000/upload', options);
+      const data = await response.json();
+      console.log(data);
+
+    } catch (err) {
+      console.error(err);
+      setError("Something didn't work please try again.");
+    }
+  };
+
+  console.log(image);
+
+  const analyzeImage = async () => {
+    if (!image) {
+      setError("Error! Must have an existing image!");
+      return
+    }
+
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: value
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+      const response = await fetch('http://localhost:8000/gemini', options);
+      const data = await response.text();
+      setResponse(data);
+    } catch (err) {
+      console.error(err);
+      setError("Something didn't work Pleat try again.");
+    }
+  }
+
+  const clear = () => {
+    setImage(null)
+    setValue("")
+    setResponse("")
+    setError("")
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='app'>
+      <section className="search-section">
+        <div className="image-container">
+          { image && <img className="image" src={URL.createObjectURL(image)}/> }
+        </div>
+        {!response && <p className="extra-info">
+          <span>
+            <label htmlFor="files">upload an image</label>
+            <input onChange={ uploadImage } id="files" accept="image/*" type="file" hidden/>
+          </span>
+          to ask questions about.
+        </p>}
+        <p>What do you want to know about the image?
+          <button className="surprise" onClick={surprise} disabled={response}>Surprise me</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="input-container">
+          <input
+            value={value}
+            placeholder="What is in the image..."
+            onChange={e => setValue(e.target.value)}
+          />
+          {(!response && !error) && <button onClick={analyzeImage}>Ask me</button>}
+          {(response || error) && <button onClick={clear}>Clear</button>}
+        </div>
+        {error && <p>{error}</p>}
+        {response && <p className="answer">{response}</p>}
+      </section>
     </div>
   );
 }
